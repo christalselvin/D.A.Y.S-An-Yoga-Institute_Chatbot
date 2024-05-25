b5ad0200bd145f842900b3d755cbf8b507a9ac3a
Christal Selvin <127867279+christalselvin@users.noreply.github.com>
2024-05-25 13:43:06 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
-"""App entry point."""
 import sys
 from flask_cors import CORS
 
-#sys.path.append("D:\\Real time Projects")
+# Ensure the current directory is in the sys.path
 sys.path.append(".")
+
 from Yoha import create_app
 
 app = create_app()
